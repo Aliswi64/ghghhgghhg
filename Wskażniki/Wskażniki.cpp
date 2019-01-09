05a08b0db6ee7033314680c8bdc92f0e01fe75e9
Alicja Świstak <swistakala@gmail.com>
2019-01-09 18:41:57 +0100

    Add new files

---
+++ Wskażniki/Wskażniki.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include"pch.h"
+
+
+using namespace std;
+
+int ile;
+
+int main()
+{
+	cout << "Ile liczb w tablicach";
+	cin >> ile;
+
+	int*tablica;
+		tablica = new int[ile];
+		for (int i = 0; i < ile; i++)
+		{
+			cout << (int) tablica << endl;
+			tablica ++;
+		}
+		delete[]tablica;
+
+
+
+	return 0;
+}
+
